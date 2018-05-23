9842bb3f571b91eee71834004c31a191bb7d6e89
root <root@localhost.localdomain>
2018-05-23 02:13:42 +0200

    style edit

--- checkcreds.php
+++ checkcreds.php
@@ -1,23 +1,22 @@
-<?php 
+<?php
 header("Access-Control-Allow-Origin: *");
-if($_SERVER['REQUEST_METHOD']==='POST'){
-    if(isset($_POST['uuid']) && $_POST['uuid']!==''){
-        $uuid = str_replace('\'','\\\'',$_POST['uuid']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['uuid']) && $_POST['uuid'] !== '') {
+        $uuid = str_replace('\'', '\\\'', $_POST['uuid']);
         include "db.php";
-        $sql = "SELECT * FROM `users` WHERE uuid='$uuid'";
-        $result = mysqli_query($db,$sql);
-        if($result===false){
+        $sql    = "SELECT * FROM `users` WHERE uuid='$uuid'";
+        $result = mysqli_query($db, $sql);
+        if ($result === false) {
             die('{"status":"error","message":"Error, please try again"}'); // '.mysqli_error($db).'"});
-        }else if(mysqli_num_rows($result)!=1){
+        } else if (mysqli_num_rows($result) != 1) {
             die('{"status":"error","message":"Invalid credentials, please log in"}');
-        }else{
+        } else {
             $row = mysqli_fetch_row($result);
-            $msg = '{"status":"success","name":"'.$row['2'].'","uuid":"'.$row['4'].'","profilePic":"'.$row['6'].'"}';
-            $sql = "UPDATE `users` SET `lastonline`='".(time()*1000)."' WHERE `uuid`=".$row['uuid'];
+            $msg = '{"status":"success","name":"' . $row['2'] . '","uuid":"' . $row['4'] . '","profilePic":"' . $row['6'] . '"}';
+            $sql = "UPDATE `users` SET `lastonline`='" . (time() * 1000) . "' WHERE `uuid`=" . $row['uuid'];
             die($msg);
         }
-
     }
-}else{
+} else {
     die('{"status":"error","message":"Invalid arguments"}');
 }
